3bebc3d8b9ad52c90b214c4b4250e2272dc32ab8
Josh Hulsey <jhul0388@gmail.com>
2017-08-09 00:01:12 -0500

    almost final

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Bacon burger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('half pounder', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('big mouth burger', false, CURRENT_TIMESTAMP);
